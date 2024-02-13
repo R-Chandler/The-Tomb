d499979281d011eb4e701b8d13978cabd68812ec
Ross Chandler <rosspchandler@gmail.com>
2024-02-13 20:52:01 +0000

    build dictionary to hold game room data and test iteration

--- run.py
+++ run.py
@@ -1,7 +1,66 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Global variables to be used throughout every room of the game.
+"""
+weapon = ""
+torch_light = 5
+inventory = []
+score = 0
 
-test = "test complete"
+"""
+Nested dictionary containing all the room descriptions, room choices and items.
+"""
 
-print(test)
+room_data = {
+    'entrance': {
+        'description': "Test Description 0",
+        'choices': ['1. East', '2. West', '3. Investigate']
+    },
+    'lower_left': {
+        'description': "Test Description 1",
+        'choices': ["1. North", "2. East", "3. Investigate"],
+    },
+    'lower_right': {
+        'description': "Test Description 2",
+        'choices': ["1. North", "2. West", "3. Investigate"]
+    },
+    'middle_left': {
+        'description': "Test Description 3",
+        'choices': ["1. North", "2. East", "3. South", "4. Investigate"]
+    },
+    'center': {
+        'description': "Test Description 4",
+        'choices': ["1. fight", "2. Flee"]
+    },
+    'middle_right': {
+        'description': "Test Description 5",
+        'choices': ["1. North", "2. South", "3. West", "4. Investigate"]
+    },
+    'upper_left': {
+        'description': "Test Description 6",
+        'choices': ["1. East", "2. South", "3. Investigate"]
+    },
+    'upper_right': {
+        'description': "Test Description 7",
+        'choices': ["1. South", "2. West", "3. Investigate"]
+    },
+    'burial_room': {
+        'description': "Test Description 8",
+        'choices': ["1. North", "2. East", "3. West", "4. Investigate"]
+    },
+    'antechamber': {
+        'description': "Test Description 9",
+        'choices': ["1. South", "2. Loot"]
+    }
+}
+
+def entrance():
+    print(room_data['entrance']['description'])
+    for choice in room_data['entrance']['choices']:
+        print(choice)
+
+
+
+entrance()
