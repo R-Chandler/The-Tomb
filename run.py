3a91e23b3a4d30ad6021a3b3fe66c422b21f00b6
Ross Chandler <rosspchandler@gmail.com>
2024-02-17 14:18:49 +0000

    add functions for lower left and lower right room

--- run.py
+++ run.py
@@ -62,11 +62,11 @@ Entrance function to handle the first room
 
 def entrance():
     global torch_light
+    run_room = True
 
-    run_game = True
+    print(room_data['entrance']['description'])
 
-    while run_game:
-        print(room_data['entrance']['description'])
+    while run_room:
         print("----------")
         for choice in room_data['entrance']['choices']:
             print(choice)
@@ -74,15 +74,71 @@ def entrance():
         entrance_response = input("What Do You Do Adventurer?:\n\n")
         if entrance_response.capitalize() == "East":
             print("\nEast Chosen")
-            break
+            lower_left()
         elif entrance_response.capitalize() == "West":
             print("\nWest Chosen")
-            break
+            lower_right()
         elif entrance_response.capitalize() == "Investigate":
             print("\nInvestigation Details\n")
             room_data['entrance'].update({'choices':['1. East', '2. West', '3. Investigate (Complete)']})
         else:
-            print("Not a valid option\n")
+            print("Not a valid option, try again!\n")
+
+"""
+lower_left function to handle the adventure in this room
+"""
+
+def lower_left():
+    global torch_light
+    run_room = True
+
+    print(room_data['lower_left']['description'])
+
+    while run_room:
+        print("----------")
+        for choice in room_data['lower_left']['choices']:
+            print(choice)
+        print("----------")
+        entrance_response = input("What Do You Do Adventurer?:\n\n")
+        if entrance_response.capitalize() == "North":
+            print("\nNorth Chosen")
+            break
+        elif entrance_response.capitalize() == "East":
+            print("\nEast Chosen")
+            entrance()
+        elif entrance_response.capitalize() == "Investigate":
+            print("\nInvestigation Details\n")
+            room_data['lower_left'].update({'choices':['1. North', '2. East', '3. Investigate (Complete)']})
+        else:
+            print("Not a valid option, try again!\n")
+
+"""
+lower_right function to handle the adventure in this room
+"""
+
+def lower_right():
+    global torch_light
+    run_room = True
+
+    print(room_data['lower_right']['description'])
+
+    while run_room:
+        print("----------")
+        for choice in room_data['lower_right']['choices']:
+            print(choice)
+        print("----------")
+        entrance_response = input("What Do You Do Adventurer?:\n\n")
+        if entrance_response.capitalize() == "North":
+            print("\nNorth Chosen")
+            break
+        elif entrance_response.capitalize() == "West":
+            print("\nWest Chosen")
+            entrance()
+        elif entrance_response.capitalize() == "Investigate":
+            print("\nInvestigation Details\n")
+            room_data['lower_right'].update({'choices':['1. North', '2. West', '3. Investigate (Complete)']})
+        else:
+            print("Not a valid option, try again!\n")
 
 
 
