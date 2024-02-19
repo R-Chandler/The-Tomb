a52a7885bf455cf9ec00fc97ea0a82a3fc305dac
Ross Chandler <rosspchandler@gmail.com>
2024-02-19 14:23:32 +0000

    made minor changes to the layout of the deployed site

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 from clear import clear
 from art import tprint
 from time import sleep
